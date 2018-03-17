e85342994a25c1af5df036d9b5248973ec745e43
HDE <hdespiritu@gmail.com>
2018-03-17 09:27:55 -0500

    try start transaction code

---
+++ config.js
@@ -0,0 +1,5 @@
+module.exports.collectionName = "testFaces";
+module.exports.region = "us-east-2";
+module.exports.accessKeyId = 'accessKeyId';
+module.exports.secretAccessKey = 'secretAccessKey';
+module.exports.apiVersion = '2016-06-27';
